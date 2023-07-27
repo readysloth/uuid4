0a2470cb2eeb3d3a823e62e3f99cef52c6e5ff74
readysloth <readysloth@protonmail.com>
2023-07-27 17:51:53 +0300

    add(uuid.h, LICENSE)

---
+++ uuid.h
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+#define UUID4_TEMPLATE "xxxxxxxx-xxxx-4xxx-xxxx-xxxxxxxxxxxx"
+#define UUID_CHAR_SET "0123456789abcdef"
+
+#define UUID4(ID, BUF, LEN) \
+  for(int _i ## ID = 0; _i ## ID < LEN && _i ## ID < sizeof(UUID4_TEMPLATE); _i ## ID ++){ \
+    (BUF)[_i ## ID] = UUID4_TEMPLATE[_i ## ID] == 'x' \
+                     ? UUID_CHAR_SET[rand() % (sizeof(UUID_CHAR_SET) - 1)] \
+                     : UUID4_TEMPLATE[_i ## ID]; \
+  }
+
+
+#define UUID4_PREDICTABLE(ID, SEED, BUF, LEN) \
+  int __rand_seed ## ID = rand(); \
+  srand(SEED); \
+  UUID4(ID, BUF, LEN); \
+  srand(__rand_seed ## ID);
+
+
+
+// Create uuid inplace
+// Usage:
+//
+// char arr[sizeof(UUID_TEMPLATE)];
+// UUID(just_suffix, arr, sizeof(arr));
